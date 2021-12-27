5a1a4fa1f958a36874c7e622838c8a1b7a260442
Henry Weigand <henryweigand10@gmail.com>
2021-12-26 17:09:50 -0800

    created all files, started work on all aspests, wip

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var questionsArray = new Map([
+    ["How many countries does China border?", "What is the deepest lake in the world?", "x"],
+    ["Which of the following South American countries is landlocked", "z"],
+    ["Which of the following European countries is double-landlocked (all of its neighbors are also landlocked)?", "q"],
+    ["Which country has the most coastline?", "t"],
+    ["What is the capital of Switzerland?", "i"],
+    ["What percentage of the world's population lives in the Southern Hemisphere?", "werw"],
+    ["which of the following countries does NOT have penguins?", "c"],
+    ["What percentage of Africa's landmass is in the northern hemisphere?", "2"],
+
+
+]);
+
+var highScores = new Map();
+
+var rankingsList = document.getElementById("rankings");
+var newScore = document.createElement("li");
+  newScore.textContent = "21";
+
+rankings.appendChild(newScore);
+console.log(rankings)
+
+var questionNo
