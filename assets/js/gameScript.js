69305eb0fa62eb608a8d45b6875179fd2de79ed8
Henry Weigand <henryweigand10@gmail.com>
2021-12-30 18:22:38 -0800

    wip

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -1,7 +1,7 @@
 var questionsMap = new Map([
     ["How many countries does China border?", ["14", "12", "13", "11"]], 
     ["What is the deepest lake in the world?", ["Lake Baikal", "Crater Lake", "Lake Superior", "Caspian See"]],
-    ["Which one of the following South American countries is landlocked", ["Paraguay", "Uruguay", "Equador", "Peru"]],
+    ["Which one of the following South American countries is landlocked?", ["Paraguay", "Uruguay", "Equador", "Peru"]],
     ["Which one of the following European countries is double-landlocked (all of its neighbors are also landlocked)?", ["Liechtenstein", "Andorra", "Luxembourg", "Slovakia"]],
     ["What is the capital of Switzerland?", ["Bern", "Zurich", "Geneva", "Basel"]],
     ["What portion of the world's population lives in the Southern Hemisphere?", ["12%", "16%", "20%", "8%"]],
@@ -10,6 +10,7 @@ var questionsMap = new Map([
     
 ]);
 
+// global variables
 var timer = document.getElementById("timer");
 var qNumber = 1;
 console.log(qNumber);
@@ -18,45 +19,23 @@ var answers = [];
 var secondsLeft;
 var timerInterval;
 
-document.getElementById("button1").addEventListener("click", function() {
-  compare(this.textContent);
-  }
-);
-
-document.getElementById("button2").addEventListener("click", function() {
-  compare(this.textContent);
-  }
-);
-
-document.getElementById("button3").addEventListener("click", function() {
-  compare(this.textContent);
-  }
-);
-
-document.getElementById("button4").addEventListener("click", function() {
-  compare(this.textContent);
-  }
-);
-
-console.log(qNumber);
-
-function alert() {
-  setTimeout(() => {
-    
-  }, 3000);
-  document.getElementById("alert").setAttribute ("style", "color: green");
-  document.getElementById("alert").textContent = "Correct!"
+// run compare function on click of the answer buttons
+for (let i = 0; i < 4; i++) {
+  document.getElementById("button"+(i+1)).addEventListener("click", function() {
+    compare(this.textContent);
+  });
 }
 
-
+// checks if answer is correct
 function compare(selection) {
   if (selection === answers[0]) {
     // on correct answer sets font color to green and says 'Correct!'
     document.getElementById("alert").setAttribute ("style", "color: green; opacity: 0.4");
     document.getElementById("alert").textContent = "Correct!"
+    // has alert fade after .6 seconds
     setTimeout(() => { 
       document.getElementById("alert").textContent = "";
-    }, 1500);
+    }, 750);
   } else {
     // on wrong answer sets font color to red, says 'Nope!, and deducts 10 seconds from the timer'
     document.getElementById("alert").setAttribute ("style", "color: red; opacity: 0.6"); 
@@ -64,23 +43,42 @@ function compare(selection) {
     secondsLeft -= 10;
     setTimeout(() => { 
       document.getElementById("alert").textContent = "";
-    }, 1500);
+    }, 750);
   }
   // calls next question regardless
   loadQuestion();
 }
 
+// durstendfeld shuffle from stack
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array;
+}
+
 function loadQuestion() {
-  if (qNumber == 9) {
+  // end game after last q
+  if (qNumber > questionsMap.size) {
     endGame();
   } else {
+    // set question and number
     question = Array.from(questionsMap.keys())[qNumber-1];
-    answers = questionsMap.get(question);
     document.getElementById("qNumber").textContent = "Question #" + qNumber;
     document.getElementById("question").textContent = question;
     qNumber++;
+    // get answers
+    answers = questionsMap.get(question);
+    console.log(answers);
+    // shuffle choices
+    var shuffledAnswers = answers.slice(0);
+    shuffleArray(shuffledAnswers);
+    // put choices in the buttons
     for (let i = 0; i < 4; i++) {
-      document.getElementById("button"+(i+1)).textContent = answers[i]; 
+      document.getElementById("button"+(i+1)).textContent = shuffledAnswers[i]; 
     }
   }
 }
@@ -103,26 +101,37 @@ function setTime(time) {
 
 
 function endGame() {
-  // clears questions and buttons
-  document.getElementById("qNumber").textContent = "";
-  document.getElementById("question").textContent = "Congrats, you scored " + secondsLeft + " points!";
-  document.getElementById("sub-prompt").textContent = "Please enter your initials";
+  // clears questions, buttons, and timer
   document.getElementById("choices").remove();
   document.getElementById("timer").remove();
   clearInterval(timerInterval);
+  document.getElementById("qNumber").textContent = "";
+  // posts centered endgame message and ask for initials
+  document.getElementById("question").textContent = "Congrats, you scored " + secondsLeft + " points!";
+  document.getElementById("sub-prompt").textContent = "Please enter your initials";
+  document.getElementById("prompts").setAttribute("style", "text-align: center")
+  // call getInitials funct
   getInitials();
 }
 
 function getInitials() {
-  var section = document.querySelector("main")
+  // get main section to attach to
+  var section = document.querySelector("main");
+  // create label
   var label = document.createElement("label");
   section.appendChild(label);
-  label.textContent = "Initials";
+  label.textContent = "Initials: ";
   label.setAttribute("for", intialsInput);
+  // create input
   var intialsInput = document.createElement("input");
   section.appendChild(intialsInput);
   intialsInput.setAttribute("type", "text");
-  intialsInput.setAttribute("value", "ABC");
+  // create submit button
+  var submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("value", "Submit");
+  submit.setAttribute("style", "margin: 10px");
+  section.appendChild(submit);
 }
 
 
